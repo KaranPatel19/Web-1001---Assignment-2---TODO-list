6a039c19f07952bc90921381d051a07724e35076
KaranPatel19 <41769712+KaranPatel19@users.noreply.github.com>
2022-11-16 22:40:06 -0500

    Made view, models and controllers and also made Dbcontext file

---
+++ todo/Models/todolist.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace todo.Models;
+
+public class todolist
+{
+    public int Id { get; set; }
+        [Required]
+        public string content { get; set; }
+
+
+
+    
+}
+    
---
+++ todo/database/todocontext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using todo.Models;
+
+namespace todo.database
+{
+	public class todocontext : DbContext
+	{
+		public todocontext(DbContextOptions<todocontext> options)
+			:base(options)
+		{
+		}
+
+		public DbSet<todolist> todo { get; set; }
+	}
+}
+
